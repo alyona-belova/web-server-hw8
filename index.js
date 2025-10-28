1442b42ff1521f330762a1e35a523766494e0e13
Alyona Belova <55944529+alyona-belova@users.noreply.github.com>
2025-10-28 16:53:27 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { createReadStream } from 'fs';
-import crypto from 'crypto';
-import http from 'http';
-import appSrc from './app.js';
+import express from "express";
+import { createReadStream } from "fs";
+import crypto from "crypto";
+import http from "http";
+import bodyParser from "body-parser";
+
+import appSrc from "./app.js";
 
 const app = appSrc(express, bodyParser, createReadStream, crypto, http);
 
-app.listen(process.env.PORT);
+app.listen(3000);
